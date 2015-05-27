98f2bd9573d60568bdef199a6e94bc0e1b7da8f3
douglas waugh <douglas.waugh.e3@gmail.com>
2015-05-27 20:51:15 +0100

    Add first test "Should display price when product is found"

---
+++ point-of-sale.tests/PointOfSaleTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace point_of_sale.tests
+{
+    [TestFixture]
+    public class PointOfSaleTests
+    {
+        [Test]
+        public void Should_display_price_when_product_is_found()
+        {
+            string sendToScreen = String.Empty;
+
+            var pointOfSale = new PointOfSale(sendToScreen);
+
+            pointOfSale.OnBarcode("12341234");
+
+            Assert.That(sendToScreen, Is.EqualTo("£9.95"));
+        }
+    }
+
+    public class PointOfSale
+    {
+        public PointOfSale(string screen)
+        {
+        }
+
+        public void OnBarcode(string barcode)
+        {
+        }
+    }
+}
