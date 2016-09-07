204fb1e655cae6c9380eca22caa0e10d5703d3b5
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-09-07 13:44:35 +0100

    Refactor to store scanned products and sum on total

--- PointOfSale/Till.cs
+++ PointOfSale/Till.cs
@@ -1,15 +1,20 @@
-﻿namespace PointOfSale
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PointOfSale
 {
     public class Till
     {
         private readonly Display _display;
         private readonly DictionaryCatalogue _catalogue;
         private decimal _total;
+        private readonly List<KeyValuePair<string, decimal>> _scannedProducts;
 
         public Till(Display display, DictionaryCatalogue dictionaryCatalogue)
         {
             _display = display;
             _catalogue = dictionaryCatalogue;
+            _scannedProducts = new List<KeyValuePair<string, decimal>>();
         }
 
         public void OnBarcode(string barcode)
@@ -23,6 +28,7 @@
             if (_catalogue.ProductsContains(barcode))
             {
                 var price = _catalogue.FindPriceForProduct(barcode);
+                _scannedProducts.Add(new KeyValuePair<string, decimal>(barcode, price));
                 _total += price;
                 _display.DisplayPrice(price);
             }
@@ -36,7 +42,7 @@
         {
             var saleInProgress = _total != 0m;
             if (saleInProgress)
-                _display.DisplayTotal(_total);
+                _display.DisplayTotal(_scannedProducts.Sum(p => p.Value));
             else
                 _display.DisplayNoSaleInProgressMessage();
         }
