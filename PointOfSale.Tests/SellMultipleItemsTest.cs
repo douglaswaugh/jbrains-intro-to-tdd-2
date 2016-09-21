6803eee440aec1bba8ddf2b6027964fe980eae0f
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-09-21 13:02:31 +0100

    Revert back to SetUp for mechanics and use Dictionary.Remove to reveal intent

--- PointOfSale.Tests/SellMultipleItemsTest.cs
+++ PointOfSale.Tests/SellMultipleItemsTest.cs
@@ -7,134 +7,80 @@ namespace PointOfSale.Tests
     [TestFixture]
     public class SellMultipleItemsTest
     {
+        private Screen _screen;
+        private Dictionary<string, decimal> _products;
+        private Till _till;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _screen = Substitute.For<Screen>();
+            _products = new Dictionary<string, decimal>();
+            var catalogue = new DictionaryCatalogue(_products);
+            _till = new Till(
+                new Display(_screen),
+                catalogue
+                );
+        }
+
         [Test]
         public void Should_display_no_sale_message_when_no_products_have_been_scanned()
         {
-            var screen = Substitute.For<Screen>();
-            var till = CreateTill(
-                screen,
-                AnyCatalogue()
-            );
+            _till.OnTotal();
 
-            till.OnTotal();
-
-            screen.Received().Print("No sale in progress. Try scanning a product.");
+            _screen.Received().Print("No sale in progress. Try scanning a product.");
         }
 
         [Test]
         public void Should_process_selling_one_found_item()
         {
-            var screen = Substitute.For<Screen>();
-            var till = CreateTill(
-                screen, 
-                CatalogueWithItem("1", 6.50m)
-            );
+            _products.Add("1", 6.50m);
 
-            till.OnBarcode("1");
-            till.OnTotal();
+            _till.OnBarcode("1");
+            _till.OnTotal();
 
-            screen.Received().Print("Total: £6.50");
+            _screen.Received().Print("Total: £6.50");
         }
 
         [Test]
         public void Should_process_selling_one_not_found_item()
         {
-            var screen = Substitute.For<Screen>();
-            var till = CreateTill(
-                screen,
-                CatalogueWithoutItem("product you won't find")
-            );
+            _products.Remove("product you won't find");
 
-            till.OnBarcode("product you won't find");
-            till.OnTotal();
+            _till.OnBarcode("product you won't find");
+            _till.OnTotal();
 
-            screen.Received().Print("No sale in progress. Try scanning a product.");
+            _screen.Received().Print("No sale in progress. Try scanning a product.");
         }
 
         [Test]
         public void Should_process_selling_multiple_found_items()
         {
-            var screen = Substitute.For<Screen>();
-            var till = CreateTill(
-                screen, 
-                CatalogueWithItems(new Dictionary<string, decimal>
-                {
-                    {"1", 8.50m},
-                    {"2", 12.75m},
-                    {"3", 3.30m}
-                })
-            );
+            _products.Add("1", 8.50m);
+            _products.Add("2", 12.75m);
+            _products.Add("3", 3.30m);
 
-            till.OnBarcode("1");
-            till.OnBarcode("2");
-            till.OnBarcode("3");
-            till.OnTotal();
+            _till.OnBarcode("1");
+            _till.OnBarcode("2");
+            _till.OnBarcode("3");
+            _till.OnTotal();
 
-            screen.Received().Print("Total: £24.55");
+            _screen.Received().Print("Total: £24.55");
         }
 
         [Test]
         public void Should_process_selling_multiple_not_found_items()
         {
-            var screen = Substitute.For<Screen>();
-            var till = CreateTill(
-                screen,
-                CatalogueWithoutItems(
-                    "product you won't find",
-                    "another product you won't find",
-                    "a thrid product you won't find"
-                )
-            );
+            _products.Remove("product you won't find");
+            _products.Remove("another product you won't find");
+            _products.Remove("a thrid product you won't find");
 
-            till.OnBarcode("product you won't find");
-            till.OnBarcode("another product you won't find");
-            till.OnBarcode("a thrid product you won't find");
-            till.OnTotal();
+            _till.OnBarcode("product you won't find");
+            _till.OnBarcode("another product you won't find");
+            _till.OnBarcode("a thrid product you won't find");
+            _till.OnTotal();
 
-            screen.Received().Print("No sale in progress. Try scanning a product.");
-        }
-
-        private DictionaryCatalogue AnyCatalogue()
-        {
-            return EmptyCatalogue();
-        }
-
-        private DictionaryCatalogue CatalogueWithItem(string barcode, decimal price)
-        {
-            var dictionary = new Dictionary<string, decimal>
-            {
-                {barcode, price}
-            };
-
-            return new DictionaryCatalogue(dictionary);
-        }
-
-        private DictionaryCatalogue CatalogueWithItems(Dictionary<string, decimal> pricesByBarcode)
-        {
-            return new DictionaryCatalogue(pricesByBarcode);
-        }
-
-        private DictionaryCatalogue CatalogueWithoutItem(string barcode)
-        {
-            return EmptyCatalogue();
-        }
-
-        private DictionaryCatalogue CatalogueWithoutItems(params string[] products)
-        {
-            return EmptyCatalogue();
-        }
-
-        private Till CreateTill(Screen screen, DictionaryCatalogue catalogueWithItem)
-        {
-            return new Till(
-                new Display(screen),
-                catalogueWithItem
-            );
-        }
-
-        private static DictionaryCatalogue EmptyCatalogue()
-        {
-            return new DictionaryCatalogue(new Dictionary<string, decimal>());
+            _screen.Received().Print("No sale in progress. Try scanning a product.");
         }
     }
 }
