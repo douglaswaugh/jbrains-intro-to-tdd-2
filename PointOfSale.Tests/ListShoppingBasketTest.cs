4affdb76a8aa06351ea9ffa00205ccc867bfdd08
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-09-21 16:51:16 +0100

    ListShoppingBasket calculates total for multiple products

--- PointOfSale.Tests/ListShoppingBasketTest.cs
+++ PointOfSale.Tests/ListShoppingBasketTest.cs
@@ -23,5 +23,17 @@ namespace PointOfSale.Tests
 
             Assert.That(basket.Total, Is.EqualTo(6.50m));
         }
+
+        [Test]
+        public void Should_calculate_total_for_multiple_products()
+        {
+            var basket = new ListShoppingBasket();
+
+            basket.AddProduct(new KeyValuePair<string, decimal>("1", 12.35m));
+            basket.AddProduct(new KeyValuePair<string, decimal>("2", 8.49m));
+            basket.AddProduct(new KeyValuePair<string, decimal>("3", 0.99m));
+
+            Assert.That(basket.Total, Is.EqualTo(21.83m));
+        }
     }
 }
