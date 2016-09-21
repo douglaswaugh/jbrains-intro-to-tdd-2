b9e2f24dfb70f68a4ca30104141a295e41a7b3f6
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-09-21 16:18:38 +0100

    ListShoppingBasket calculates total for one product

--- PointOfSale.Tests/ListShoppingBasketTest.cs
+++ PointOfSale.Tests/ListShoppingBasketTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace PointOfSale.Tests
 {
@@ -11,6 +12,16 @@ namespace PointOfSale.Tests
             var basket = new ListShoppingBasket();
 
             Assert.That(basket.Total, Is.EqualTo(0m));
+        }
+
+        [Test]
+        public void Should_calculate_total_for_one_product()
+        {
+            var basket = new ListShoppingBasket();
+
+            basket.AddProduct(new KeyValuePair<string, decimal>("1", 6.50m));
+
+            Assert.That(basket.Total, Is.EqualTo(6.50m));
         }
     }
 }
