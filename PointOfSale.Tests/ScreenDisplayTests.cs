80182fd5ba39016324e0fd9b08ddfacb92fa4ce2
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-09-21 17:11:05 +0100

    ScreenDisplay displays product not found message on its Screen

--- PointOfSale.Tests/ScreenDisplayTests.cs
+++ PointOfSale.Tests/ScreenDisplayTests.cs
@@ -16,5 +16,16 @@ namespace PointOfSale.Tests
 
             screen.Received().Print("£3.50");
         }
+
+        [Test]
+        public void Should_display_product_not_found_message_on_screen()
+        {
+            var screen = Substitute.For<Screen>();
+            var display = new ScreenDisplay(screen);
+
+            display.DisplayProductNotFoundMessage("not found product barcode");
+
+            screen.Received().Print("Product not found for not found product barcode");
+        }
     }
 }
