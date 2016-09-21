f77a375eeb7d8b2171a4c00d929ceda51f7e702b
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-09-21 12:44:27 +0100

    Remove Setup in multiple items test in favour of intention revealing methods

--- PointOfSale.Tests/SellMultipleItemsTest.cs
+++ PointOfSale.Tests/SellMultipleItemsTest.cs
@@ -7,72 +7,129 @@ namespace PointOfSale.Tests
     [TestFixture]
     public class SellMultipleItemsTest
     {
-        private Screen _screen;
-        private Till _till;
-        private Dictionary<string, decimal> _pricesByBarcode;
-
-        [SetUp]
-        public void SetUp()
-        {
-            _screen = Substitute.For<Screen>();
-            _pricesByBarcode = new Dictionary<string, decimal>();
-            _till = new Till(
-                new Display(_screen), 
-                new DictionaryCatalogue(_pricesByBarcode));
-        }
-
         [Test]
         public void Should_display_no_sale_message_when_no_products_have_been_scanned()
         {
-            _till.OnTotal();
+            var screen = Substitute.For<Screen>();
+            var till = CreateTill(
+                screen,
+                AnyCatalogue()
+            );
 
-            _screen.Received().Print("No sale in progress. Try scanning a product.");
+            till.OnTotal();
+
+            screen.Received().Print("No sale in progress. Try scanning a product.");
         }
 
         [Test]
         public void Should_process_selling_one_found_item()
         {
-            _pricesByBarcode.Add("123245678", 6.50m);
+            var screen = Substitute.For<Screen>();
+            var till = CreateTill(
+                screen, 
+                CatalogueWithItem("123245678", 6.50m)
+            );
 
-            _till.OnBarcode("123245678");
-            _till.OnTotal();
+            till.OnBarcode("123245678");
+            till.OnTotal();
 
-            _screen.Received().Print("Total: £6.50");
+            screen.Received().Print("Total: £6.50");
         }
 
         [Test]
         public void Should_process_selling_one_not_found_item()
         {
-            _till.OnBarcode("123245678");
-            _till.OnTotal();
+            var screen = Substitute.For<Screen>();
+            var till = CreateTill(
+                screen,
+                CatalogueWithoutItem("123245678")
+            );
 
-            _screen.Received().Print("No sale in progress. Try scanning a product.");
+            till.OnBarcode("123245678");
+            till.OnTotal();
+
+            screen.Received().Print("No sale in progress. Try scanning a product.");
         }
 
         [Test]
         public void Should_process_selling_multiple_found_items()
         {
-            _pricesByBarcode.Add("12341234", 8.50m);
-            _pricesByBarcode.Add("43214321", 12.75m);
-            _pricesByBarcode.Add("12121212", 3.30m);
+            var screen = Substitute.For<Screen>();
+            var till = CreateTill(
+                screen, 
+                CatalogueWithItems(new Dictionary<string, decimal>
+                {
+                    {"12341234", 8.50m},
+                    {"43214321", 12.75m},
+                    {"12121212", 3.30m}
+                })
+            );
 
-            _till.OnBarcode("12341234");
-            _till.OnBarcode("43214321");
-            _till.OnBarcode("12121212");
-            _till.OnTotal();
+            till.OnBarcode("12341234");
+            till.OnBarcode("43214321");
+            till.OnBarcode("12121212");
+            till.OnTotal();
 
-            _screen.Received().Print("Total: £24.55");
+            screen.Received().Print("Total: £24.55");
         }
 
         [Test]
         public void Should_process_selling_multiple_not_found_items()
         {
-            _till.OnBarcode("product you won't find");
-            _till.OnBarcode("another product you won't find");
-            _till.OnBarcode("a thrid product you won't find");
-            _till.OnTotal();
+            var screen = Substitute.For<Screen>();
+            var till = CreateTill(
+                screen,
+                CatalogueWithoutItems(
+                    "product you won't find",
+                    "another product you won't find",
+                    "a thrid product you won't find"
+                )
+            );
 
-            _screen.Received().Print("No sale in progress. Try scanning a product.");
+            till.OnBarcode("product you won't find");
+            till.OnBarcode("another product you won't find");
+            till.OnBarcode("a thrid product you won't find");
+            till.OnTotal();
+
+            screen.Received().Print("No sale in progress. Try scanning a product.");
+        }
+
+        private Till CreateTill(Screen screen, DictionaryCatalogue catalogueWithItem)
+        {
+            return new Till(
+                new Display(screen),
+                catalogueWithItem
+            );
+        }
+
+        private DictionaryCatalogue AnyCatalogue()
+        {
+            return new DictionaryCatalogue(new Dictionary<string, decimal>());
+        }
+
+        private DictionaryCatalogue CatalogueWithItem(string barcode, decimal price)
+        {
+            var dictionary = new Dictionary<string, decimal>
+            {
+                {barcode, price}
+            };
+
+            return new DictionaryCatalogue(dictionary);
+        }
+
+        private DictionaryCatalogue CatalogueWithItems(Dictionary<string, decimal> pricesByBarcode)
+        {
+            return new DictionaryCatalogue(pricesByBarcode);
+        }
+
+        private DictionaryCatalogue CatalogueWithoutItem(string barcode)
+        {
+            return new DictionaryCatalogue(new Dictionary<string, decimal>());
+        }
+
+        private DictionaryCatalogue CatalogueWithoutItems(params string[] products)
+        {
+            return new DictionaryCatalogue(new Dictionary<string, decimal>());
         }
     }
 }
