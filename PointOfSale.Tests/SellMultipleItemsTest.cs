2adbf2f4d7318f1767bb45a5228806605c204cce
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-09-21 16:56:31 +0100

    Send amount returned from ShoppingBasketEmpty to Display

--- PointOfSale.Tests/SellMultipleItemsTest.cs
+++ PointOfSale.Tests/SellMultipleItemsTest.cs
@@ -8,24 +8,25 @@ namespace PointOfSale.Tests
     public class SellMultipleItemsTest
     {
         private Screen _screen;
-        private Dictionary<string, decimal> _products;
         private Till _till;
+        private ShoppingBasket _shoppingBasket;
 
         [SetUp]
         public void SetUp()
         {
             _screen = Substitute.For<Screen>();
-            _products = new Dictionary<string, decimal>();
-            var catalogue = new DictionaryCatalogue(_products);
+            _shoppingBasket = Substitute.For<ShoppingBasket>();
             _till = new Till(
                 new Display(_screen),
-                catalogue,
-                new ListShoppingBasket());
+                new DictionaryCatalogue(new Dictionary<string, decimal>()),
+                _shoppingBasket);
         }
 
         [Test]
         public void Should_display_no_sale_message_when_no_products_have_been_scanned()
         {
+            _shoppingBasket.Empty.Returns(true);
+
             _till.OnTotal();
 
             _screen.Received().Print("No sale in progress. Try scanning a product.");
@@ -34,20 +35,9 @@ namespace PointOfSale.Tests
         [Test]
         public void Should_send_basket_total_to_display_on_total()
         {
-            _products.Add("1", 6.50m);
+            _shoppingBasket.Empty.Returns(false);
+            _shoppingBasket.Total.Returns(6.50m);
 
-            _till.OnBarcode("1");
-            _till.OnTotal();
-
-            _screen.Received().Print("Total: £6.50");
-        }
-
-        [Test]
-        public void Should_process_selling_one_found_product()
-        {
-            _products.Add("1", 6.50m);
-
-            _till.OnBarcode("1");
             _till.OnTotal();
 
             _screen.Received().Print("Total: £6.50");
