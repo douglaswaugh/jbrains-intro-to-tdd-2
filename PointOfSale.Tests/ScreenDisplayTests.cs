61d620b74361b2744f412425baa79f0e19163eff
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-09-21 17:05:24 +0100

    ScreenDisplay displays a price on its Screen

---
+++ PointOfSale.Tests/ScreenDisplayTests.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace PointOfSale.Tests
+{
+    [TestFixture]
+    public class ScreenDisplayTests
+    {
+        [Test]
+        public void Should_display_price_on_screen()
+        {
+            var screen = Substitute.For<Screen>();
+            var display = new ScreenDisplay(screen);
+
+            display.DisplayPrice(3.50m);
+
+            screen.Received().Print("£3.50");
+        }
+    }
+}
